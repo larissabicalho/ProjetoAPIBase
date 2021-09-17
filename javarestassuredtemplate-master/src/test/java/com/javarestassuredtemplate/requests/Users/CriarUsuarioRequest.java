c15128d4c6a1cbda4ea882bbea7e692e1894ddd4
larissabicalho <lari_nazarebicalho@hotmail.com>
2021-09-17 12:08:32 -0300

    correcoes

--- javarestassuredtemplate-master/src/test/java/com/javarestassuredtemplate/requests/Users/CriarUsuarioRequest.java
+++ javarestassuredtemplate-master/src/test/java/com/javarestassuredtemplate/requests/Users/CriarUsuarioRequest.java
@@ -1,12 +1,19 @@
+package com.javarestassuredtemplate.requests.Users;
+
+import com.javarestassuredtemplate.bases.RequestRestBase;
+import com.javarestassuredtemplate.jsonObjects.Users.CriarUsuario;
+import io.restassured.http.Method;
+
 public class CriarUsuarioRequest extends RequestRestBase {
-    public PostCriarUsuarioRequest() {
+    public CriarUsuarioRequest() {
         requestService = "/api/rest/users/";
         method = Method.POST;
-        headers.put("Authorization", "wgGoYvqRl2f-iAYzz4TPW5EiqkRvyFNI");
-    }
+        headers.put("Authorization", "cMyRDGhiz3P8Z4p1cip_0J38eUm1g1cV");
 
+    }
     public void setJsonBodyUsingJavaObject(CriarUsuario criarUsuario) {
         jsonBody = criarUsuario;
+
     }
 
 }
