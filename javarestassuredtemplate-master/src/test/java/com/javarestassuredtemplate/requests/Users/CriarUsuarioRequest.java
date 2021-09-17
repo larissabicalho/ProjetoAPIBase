dbffc3e1aff969e65dab0223659eec1094abc147
larissabicalho <lari_nazarebicalho@hotmail.com>
2021-09-17 20:01:22 -0300

    continuacao

--- javarestassuredtemplate-master/src/test/java/com/javarestassuredtemplate/requests/Users/CriarUsuarioRequest.java
+++ javarestassuredtemplate-master/src/test/java/com/javarestassuredtemplate/requests/Users/CriarUsuarioRequest.java
@@ -4,13 +4,18 @@ import com.javarestassuredtemplate.bases.RequestRestBase;
 import com.javarestassuredtemplate.jsonObjects.Users.CriarUsuario;
 import io.restassured.http.Method;
 
+
+
+
 public class CriarUsuarioRequest extends RequestRestBase {
-    public CriarUsuarioRequest() {
+
+    public CriarUsuarioRequest(){
         requestService = "/api/rest/users/";
         method = Method.POST;
         headers.put("Authorization", "cMyRDGhiz3P8Z4p1cip_0J38eUm1g1cV");
-
+        headers.put("Content-Type", "application/json");
     }
+
     public void setJsonBodyUsingJavaObject(CriarUsuario criarUsuario) {
         jsonBody = criarUsuario;
 
--- javarestassuredtemplate-master/src/test/java/com/javarestassuredtemplate/tests/Users/CriarUsuarioTests.java
+++ javarestassuredtemplate-master/src/test/java/com/javarestassuredtemplate/tests/Users/CriarUsuarioTests.java
@@ -1,9 +1,35 @@
 package com.javarestassuredtemplate.tests.Users;
 
 import com.javarestassuredtemplate.bases.TestBase;
+import com.javarestassuredtemplate.defaultParameters.GlobalStaticParameters;
+import com.javarestassuredtemplate.jsonObjects.Users.CriarUsuario;
+import com.javarestassuredtemplate.requests.Users.CriarUsuarioRequest;
+import io.restassured.response.ValidatableResponse;
+import org.apache.http.HttpStatus;
 import org.testng.annotations.Test;
+
 
 @Test
 public class CriarUsuarioTests extends TestBase {
+
+    CriarUsuarioRequest criarUsuarioRequest;
+    ValidatableResponse response;
+    int statusCodeEsperado = HttpStatus.SC_OK;
+    GlobalStaticParameters globalStaticParameters;
+
+    public void criarUsuarioSucesso(){
+        //Busca dados do usuario
+        CriarUsuario buscaDadosUsuario = new CriarUsuario();
+        //fluxo
+        criarUsuarioRequest = new CriarUsuarioRequest();
+        criarUsuarioRequest.setJsonBodyUsingJavaObject(buscaDadosUsuario);
+        response = criarUsuarioRequest.executeRequest();
+        //Validações
+        response.log().all();
+        response.statusCode(statusCodeEsperado);
+        
+
+    }
+
 
 }
